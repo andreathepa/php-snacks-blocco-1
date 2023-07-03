8f23b9a46d61d6d572fb3ee9d1b3e46362dd96fb
andreathepa <andreadepa32@gmail.com>
2023-07-03 19:50:14 +0200

    snack 1 done

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+    
+    $lba = [
+        [
+            'teams'  => 'Brindisi - Trieste',
+            'result' => '90-40'
+        ],
+        [
+            'teams'  => 'Olimpia - Milano',
+            'result' => '80-50'
+        ],
+        [
+            'teams'  => 'Sassari - Pesaro',
+            'result' => '75-49'
+        ],
+        [
+            'teams'  => 'Tortona - Reggiana',
+            'result' => '56-89'
+        ],
+        [
+            'teams'  => 'Trento - Scafati',
+            'result' => '97-48'
+        ],
+        [
+            'teams'  => 'Brescia - Venezia',
+            'result' => '52-78'
+        ]        
+        ];
+
+    $basket = [
+        [
+            'home'      => 'Brindisi',
+            'guest'     => 'Trieste',
+            'result'    => '85-40'
+        ],
+        [
+            'home'      => 'Olimpia',
+            'guest'     => 'Milano',
+            'result'    => '90-105'
+        ],
+        [
+            'home'      => 'Sassari',
+            'guest'     => 'Pesaro',
+            'result'    => '90-85'
+        ],
+        [
+            'home'      => 'Tortona',
+            'guest'     => 'Reggiana',
+            'result'    => '59-70'
+        ],
+        ];
+
+
+       
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack php</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <ul>
+                    <?php foreach($lba as $game) { ?>
+                        <li>
+                            <?php echo ($game['teams']." ".$game['result']); ?>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+            <div class="col-12">
+                <ul>
+                    <?php foreach($basket as $data) { ?>
+                        <li>
+                            <?php echo ($data['home']." - ".$data['guest'].": ".$data['result']); ?>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
